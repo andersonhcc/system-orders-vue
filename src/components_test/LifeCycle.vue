93ed328c0116cf3e41492669d5856f001dd71df3
Anderson Henrique <103274266+andersonhcc@users.noreply.github.com>
2023-11-22 22:43:04 -0300

    first commit

---
+++ src/components_test/LifeCycle.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ name }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "LyfeCycle",
+        data () {
+            return {
+                name: "Anderson",
+            }
+        },
+        created(){
+
+        }
+    }
+
+</script>
